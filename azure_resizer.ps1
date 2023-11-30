e58c718c350c32a05b014a7336fc3c44b51c70b3
BillPelegrini <contato@brunopelegrini.com>
2023-11-30 12:23:46 -0300

    add resize script

---
+++ azure_resizer.ps1
@@ -0,0 +1,22 @@
+# Connects to Azure using a managed identity
+Connect-AzAccount -Identity 
+
+# Disables warnings
+Update-AzConfig -DisplayBreakingChangeWarning $false
+
+# Imports necessary modules
+Import-Module Az.Sql
+
+# Defines environment variables
+$resourceGroupName = "Homologation"
+$serverName = "homolog"
+$databaseName = "prerelease"
+$edition = "Premium"
+$requestedServiceObjectiveName = "P1"
+
+# Resizes the database
+Set-AzSqlDatabase -ResourceGroupName $resourceGroupName -ServerName $serverName -DatabaseName $databaseName -Edition $edition -RequestedServiceObjectiveName $requestedServiceObjectiveName
+
+# Prints a success message
+Write-Host "The database $databaseName has been successfully resized to $edition/$requestedServiceObjectiveName"
+
